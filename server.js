dfd2dc62e84d7363fc123a052fadd330c4af6a19
clairecleverlamb <clairechenszeying@gmail.com>
2025-04-07 17:10:00 -0700

    added day controller

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const logger = require('morgan');
 const testJwtRouter = require('./controllers/test-jwt');
 const authRouter = require('./controllers/auth');
 const usersRouter = require('./controllers/users');
-const mealsRouter = require('./controllers/meals')
-
+const mealsRouter = require('./controllers/meals');
+const daysRouter = require('./controllers/days');
 const verifyToken = require('./middleware/verify-token');
 
 dotenv.config();
@@ -29,6 +29,7 @@ app.use('/auth', authRouter);
 app.use('/users', verifyToken, usersRouter);
 app.use('/test-jwt', testJwtRouter);
 app.use('/meals', verifyToken, mealsRouter);
+app.use('/days', verifyToken, daysRouter);
 
 app.listen(3000, () => {
     console.log('The express app is ready!');

86074eb3099458ae8ab91baa0b5197cda455e6bd
Svetlana Kovakina <skovakina@gmail.com>
2025-04-10 21:21:52 -0700

    feat: update model

--- models/meal.js
+++ models/meal.js
@@ -1,18 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const mealSchema = mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    note: {
-        type: String
-    },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    },
+  title: {
+    type: String,
+    required: true,
+  },
+  note: {
+    type: String,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  column: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Day",
+    default: null,
+  },
+  mealType: {
+    type: String,
+    enum: [
+      "breakfast",
+      "lunch",
+      "dinner",
+      "firstSnack",
+      "secondSnack",
+      "unassigned",
+    ],
+    default: "unassigned",
+  },
 });
 
-const Meal = mongoose.model('Meal', mealSchema);
+const Meal = mongoose.model("Meal", mealSchema);
 module.exports = Meal;
